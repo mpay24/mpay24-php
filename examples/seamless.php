d1f62f43fff1e986e277362cbb4154f8c8f6cf85
Tobias Lins <tobias@lins.in>
2016-11-05 17:41:02 +0100

    Update seamless.php

--- examples/seamless.php
+++ examples/seamless.php
@@ -1,14 +1,17 @@
 <?php
-include_once ("../lib/MPAY24.php");
-$shop = new MPAY24();
-$tokenizer = $shop->createPaymentToken("CC")->getPaymentResponse();
+  include_once ("../lib/MPAY24.php");
+  $shop = new MPAY24();
+  $tokenizer = $shop->createPaymentToken("CC")->getPaymentResponse();
 ?>
+
 <iframe src="<?php echo $tokenizer->location; ?>" frameBorder="0"></iframe>
+
 <form action="pay.php" method="POST">
   <input name="token" type="hidden" value="<? echo $tokenizer->token; ?>" />
   <button id="paybutton" name="type" value="TOKEN" type="submit" disabled="true">Pay with creditcard</button>
   <button name="type" value="PAYPAL" type="submit">Pay with paypal</button>
 </form>
+
 <script>
 window.addEventListener("message", checkValid, false);
 function checkValid(form) {
