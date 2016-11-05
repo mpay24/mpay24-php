454fabd0cd8dc89f54e8f2a7aabe4111a375a96a
Tobias Lins <tobias@lins.in>
2016-11-05 17:41:30 +0100

    Update pay.php

--- examples/pay.php
+++ examples/pay.php
@@ -1,24 +1,25 @@
 <?php
-include_once ("../lib/MPAY24.php");
-$shop = new MPAY24();
+  include_once ("../lib/MPAY24.php");
 
-$payment = array(
-  "amount" => "100",
-  "currency" => "EUR"
-);
+  $shop = new MPAY24();
 
-if(isset($_POST["type"])) {
-  $type = $_POST["type"];
-  switch($type) {
-    case "TOKEN":
-      $payment["token"] = $_POST["token"];
-      break;
+  $payment = array(
+    "amount" => "100",
+    "currency" => "EUR"
+  );
+
+  if(isset($_POST["type"])) {
+    $type = $_POST["type"];
+    switch($type) {
+      case "TOKEN":
+        $payment["token"] = $_POST["token"];
+        break;
+    }
+    $result = $shop->acceptPayment($type, "123", $payment);
+    if($result->generalResponse->returnCode == "REDIRECT") {
+      header('Location: '.$result->location);
+    } else {
+      echo $result->generalResponse->returnCode;
+    }
   }
-  $result = $shop->acceptPayment($type, "123", $payment);
-  if($result->generalResponse->returnCode == "REDIRECT") {
-    header('Location: '.$result->location);
-  } else {
-    echo $result->generalResponse->returnCode;
-  }
-}
 ?>
