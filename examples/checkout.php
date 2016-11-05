0b19c08ff526dceea1733b7e80af9dc7373671a6
Tobias Lins <tobias@lins.in>
2016-11-05 17:41:48 +0100

    Update checkout.php

--- examples/checkout.php
+++ examples/checkout.php
@@ -1,10 +1,11 @@
 <?php
-include_once ("../lib/MPAY24.php");
-$shop = new MPAY24();
+  include_once ("../lib/MPAY24.php");
+  
+  $shop = new MPAY24();
 
-$mdxi = new ORDER();
-$mdxi->Order->Tid = "123";
-$mdxi->Order->Price = "1.00";
+  $mdxi = new ORDER();
+  $mdxi->Order->Tid = "123";
+  $mdxi->Order->Price = "1.00";
 
-header('Location: '.$shop->selectPayment($mdxi)->location);
+  header('Location: '.$shop->selectPayment($mdxi)->location);
 ?>
