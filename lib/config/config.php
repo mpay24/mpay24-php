379a82218f16c60dc6eb8ec07b5db8aa4fa227ad
Tobias Lins <tobias@lins.in>
2016-12-07 16:03:27 +0100

    reformat config

--- lib/config/config.php
+++ lib/config/config.php
@@ -1,18 +1,18 @@
 <?php
-define("MERCHANT_ID", "9****");
-define("SOAP_PASS", "**********");
-define("TEST_SYSTEM", TRUE);
-define("DEBUG", TRUE);
-define("ENABLE_CURL_LOG", FALSE);
-define("PROXY_HOST", NULL);
-define("PROXY_PORT", NULL);
-define("PROXY_USER", NULL);
-define("PROXY_PASS", NULL);
-define("VERIFY_PEER", TRUE);
+  define("MERCHANT_ID", "9****");
+  define("SOAP_PASS", "**********");
+  define("TEST_SYSTEM", TRUE);
+  define("DEBUG", TRUE);
+  define("ENABLE_CURL_LOG", FALSE);
+  define("PROXY_HOST", NULL);
+  define("PROXY_PORT", NULL);
+  define("PROXY_USER", NULL);
+  define("PROXY_PASS", NULL);
+  define("VERIFY_PEER", TRUE);
 
-define("SPID", "abcdefghjklmnop");
-define("FLEX_LINK_PASS", "**********");
+  define("SPID", "abcdefghjklmnop");
+  define("FLEX_LINK_PASS", "**********");
 
-define("CURL_LOG", "./curllog.log"); //the path starts from /lib directory.
-define("MPAY24_LOG", "./mpay24.log");
+  define("CURL_LOG", "./curllog.log"); //the path starts from /lib directory.
+  define("MPAY24_LOG", "./mpay24.log");
 ?>
