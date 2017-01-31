a019967d071b8079a8baea3a3b52e169be933fa6
Ing. Stefan Polzer <stefan@posit.at>
2017-01-31 13:01:18 +0100

    Update example file for checkout

--- examples/checkout.php
+++ examples/checkout.php
@@ -1,12 +1,15 @@
 <?php
-  require("../bootstrap.php");
-  use mPay24\MPAY24;
-  use mPay24\ORDER;
+require("../bootstrap.php");
+use mPay24\MPAY24;
+use mPay24\ORDER;
 
-  $mpay24 = new MPAY24();
+$mpay24 = new MPAY24();
 
-  $mdxi = new ORDER();
-  $mdxi->Order->Tid = "123";
-  $mdxi->Order->Price = "1.00";
+$mdxi                           = new ORDER();
+$mdxi->Order->Tid               = "123";
+$mdxi->Order->Price             = "1.00";
+$mdxi->Order->URL->Success      = 'http://yourpage.com/success';
+$mdxi->Order->URL->Error        = 'http://yourpage.com/error';
+$mdxi->Order->URL->Confirmation = 'http://yourpage.com/confirmation';
 
-  header('Location: '.$mpay24->selectPayment($mdxi)->location);
+header('Location: ' . $mpay24->selectPayment($mdxi)->location);
