e185376dea36e31cb39f994096a13d460982e769
Ing. Stefan Polzer <stefan@posit.at>
2017-01-31 13:33:37 +0100

    Update example file

--- examples/chargeprofile.php
+++ examples/chargeprofile.php
@@ -1,24 +1,26 @@
 <?php
-  require("../bootstrap.php");
-  use mPay24\MPAY24;
+require("../bootstrap.php");
+use mPay24\MPAY24;
 
-  $mpay24 = new MPAY24();
+$mpay24 = new MPAY24();
 
-  $payment = array(
-    "amount" => "100",
-    "currency" => "EUR"
-  );
+$payment = array(
+	'amount'     => '100',
+	'currency'   => 'EUR',
+);
 
-  $additional = array(
-    "customerID" => "customer123",
-    "confirmationURL" => "http://yourdomain.com/confirmation"
-  );
+$additional = array(
+	"customerID"      => "customer123",
+	"customerName"    => 'Jon Doe',
+	"confirmationURL" => "http://yourdomain.com/confirmation",
+	"order"           => ['description' => "Your description of the Order"], // Optional
+);
 
-  $type="PROFILE";
+$type = "PROFILE";
 
-  $result = $mpay24->acceptPayment($type, "123", $payment, $additional);
-  echo "Status: ".$result->generalResponse->status;
-  echo "<br>";
-  echo "ReturnCode: ".$result->generalResponse->returnCode;
-  echo "<br>";
-  echo "mPAYTID: ".$result->mpayTID;
+$result = $mpay24->acceptPayment($type, "123 TID", $payment, $additional);
+echo "Status: " . $result->generalResponse->status;
+echo "<br>";
+echo "ReturnCode: " . $result->generalResponse->returnCode;
+echo "<br>";
+echo "mPAYTID: " . $result->mpayTID;
--- examples/checkout.php
+++ examples/checkout.php
@@ -6,10 +6,10 @@ use mPay24\ORDER;
 $mpay24 = new MPAY24();
 
 $mdxi                           = new ORDER();
-$mdxi->Order->Tid               = "123";
+$mdxi->Order->Tid               = "123 TID";
 $mdxi->Order->Price             = "1.00";
-$mdxi->Order->URL->Success      = 'http://yourpage.com/success';
-$mdxi->Order->URL->Error        = 'http://yourpage.com/error';
-$mdxi->Order->URL->Confirmation = 'http://yourpage.com/confirmation';
+$mdxi->Order->URL->Success      = 'http://yourdomain.com/success';
+$mdxi->Order->URL->Error        = 'http://yourdomain.com/error';
+$mdxi->Order->URL->Confirmation = 'http://yourdomain.com/confirmation';
 
 header('Location: ' . $mpay24->selectPayment($mdxi)->location);
