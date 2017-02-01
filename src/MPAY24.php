877838e8341647afb27ecf8ab5d05f75513461c6
Ing. Stefan Polzer <stefan@posit.at>
2017-02-01 21:37:30 +0100

    Adding MPAY24 wrapper class

---
+++ src/MPAY24.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mpay24;
+
+/**
+ * Class MPAY24
+ * @package    Mpay24
+ *
+ * @author     Stefan Polzer <develop@posit.at>
+ * @filesource MPAY24.php
+ * @license    MIT
+ *
+ * @deprecated Use Mpay24Soap instated
+ */
+class MPAY24 extends Mpay24Soap
+{
+
+}
